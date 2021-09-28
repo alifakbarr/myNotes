fb1a2605f3344f132048b87f7e978267a54ef55b
alifakbarr <alifakbar8800@gmail.com>
2021-09-28 13:29:27 +0700

    layouting

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 
-
+// set up express
 const app = express()
 const port = 3000
 
+// set up express-layouts
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// page home
 app.get('/', (req,res) => {
-    
+    res.render('index',{
+        title: 'MyNotes',
+        layout: 'layouts/main-layout'
+    })
 })
 
-// if page not found
-app.use((req,res) => {
-    res.status(404).send(404)
+// page form add note
+app.get('/note/create', (req,res) => {
+    res.render('notes/create',{
+        title : 'Create My Note',
+        layout: 'layouts/main-layout'
+    })
 })
 
 // listen
